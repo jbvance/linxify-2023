6f48ce42bfa05d9c01a3ac3fdf9a281b622ebb3e
Jason Vance <jbvance@gmail.com>
2023-04-25 15:45:43 -0500

    update 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const Custom404 = () => {
+  useEffect(() => {
+    console.log('LOC', window.location.href);
+  }, []);
   return <div>404 - Not Found</div>;
 };
 
