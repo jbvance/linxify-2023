05269084a861ee0e926f4d8c0e9c8d3d71609956
Jason Vance <jbvance@gmail.com>
2023-04-25 09:34:38 -0500

    update Navigation for session detection and style links based on current page

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,3 +1,5 @@
+import { useRouter } from 'next/router';
+import { useSession, signOut, signIn } from 'next-auth/react';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
@@ -13,6 +15,9 @@ const orbitron = Orbitron({
 });
 
 const Navigation = () => {
+  const { data: session, status } = useSession();
+  const router = useRouter();
+  console.log('PATH', router.pathname);
   return (
     <Navbar bg="light" expand="lg">
       <Container fluid>
@@ -30,12 +35,31 @@ const Navigation = () => {
             style={{ maxHeight: '100px' }}
             navbarScroll
           >
-            <Link className={styles.link} href="/links">
+            <Link
+              className={`${styles.link} ${
+                router.pathname == '/links' ? styles.active_link : ''
+              }`}
+              href="/links"
+            >
               Links
             </Link>
-            <Link className={styles.link} href="/categories">
+            <Link
+              className={`${styles.link} ${
+                router.pathname == '/categories' ? styles.active_link : ''
+              }`}
+              href="/categories"
+            >
               Categories
             </Link>
+          </Nav>
+          <Nav>
+            {status === 'authenticated' ? (
+              <Button variant="success" onClick={signOut}>
+                Logout
+              </Button>
+            ) : (
+              <Button variant="success">Login</Button>
+            )}
           </Nav>
         </Navbar.Collapse>
       </Container>
